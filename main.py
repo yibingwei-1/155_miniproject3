9a71ebcd091830cf7825a62d9eec0b1e44668a37
ling <zhilingzhouzju@gmail.com>
2020-03-10 13:52:28 -0700

    random template from S

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import LSTM
 import preprocess
 import write_poems
+import random
 
 
 LSTM_Generator = LSTM.LSTMGenerator()
 
 # LSTM_Generator.train('data/shakespeare.txt')
 sample_sentence = LSTM.LSTMGenerator.sample_sentences(LSTM_Generator, 'data/shakespeare.txt')
-syllable_dict = preprocess.syllables_interpreter('data/Syllable_dictionary.txt', LSTM_Generator.get_word_to_int())
+sentence_dict = write_poems.classify_sentence(sample_sentence)
+syllable_dict = write_poems.read_syllable_from()
 
-for sentence in sample_sentence:
-    print(write_poems.truncate_sentence(sentence, LSTM_Generator.get_word_to_int(), syllable_dict))
+syllable_template = random.choice(syllable_dict)
+print(syllable_template)
 
-# print(sample_sentence)
+sonnet = []
+for i, syllable in enumerate(syllable_template):
+    sonnet.append(random.choice(sentence_dict[syllable]))
+print(sonnet)
+
